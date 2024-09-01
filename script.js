481fb283753d68f297c720f9d3d244b837397029
likhithadeverapally <dilkhitha08@gmail.com>
2024-09-01 16:49:44 +0530

    updated js file

--- script.js
+++ script.js
@@ -73,6 +73,7 @@ const init = () => {
   getNews();
   createOptions();
 };
+let apiKey = "pub_52293b25682d8afc9f197b128984a9ace2ef8";
 
 window.onload = () => {
   requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}`;
